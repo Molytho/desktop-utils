94857cbd44bf31e70736c4a743fa65b085ce15a5
Robin Ebert <ebertrobin2002@gmail.com>
2026-02-19 00:29:03 +0100

    Add localconfig option

--- keepassxc-secret-service-no-password/keepassxc-secret-service-no-password.cpp
+++ keepassxc-secret-service-no-password/keepassxc-secret-service-no-password.cpp
@@ -6,10 +6,16 @@
 
 #include <iostream>
 
-constexpr char CRED_PATH[] = "/home/robin/.config/.dbus_keepassxc";
-constexpr char DB_PATH[]   = "/home/robin/Dokumente/DBusSecretService.kdbx";
-constexpr char EXE_PATH[]  = "/usr/bin/keepassxc";
-constexpr char INI_PATH[]  = "/home/robin/.config/keepassxc/keepassxc_dbus.ini";
+#define KEY_FILE      ".dbus_keepassxc"
+#define DATABASE_FILE "DBusSecretService.kdbx"
+#define INI_FILE      "keepassxc_dbus.ini"
+#define BASE_PATH     "/home/robin/"
+
+constexpr char EXE_PATH[]   = "/usr/bin/keepassxc";
+constexpr char CRED_PATH[]  = BASE_PATH ".config/" KEY_FILE;
+constexpr char INI_PATH[]   = BASE_PATH ".config/keepassxc/" INI_FILE;
+constexpr char CACHE_PATH[] = BASE_PATH ".cache/keepassxc/" INI_FILE;
+constexpr char DB_PATH[]    = BASE_PATH "Dokumente/" DATABASE_FILE;
 
 void open_cred_file() {
     int cred_fd = open(CRED_PATH, O_RDONLY | O_CLOEXEC | O_NOFOLLOW);
@@ -26,7 +32,7 @@ void open_cred_file() {
 }
 
 [[noreturn]] void spawn_keepass() {
-    execl(EXE_PATH, EXE_PATH, "--pw-stdin", "--config", INI_PATH, DB_PATH, NULL);
+    execl(EXE_PATH, EXE_PATH, "--pw-stdin", "--config", INI_PATH, "--localconfig", CACHE_PATH, DB_PATH, NULL);
     perror("spawn_keepass");
     exit(3);
 }
